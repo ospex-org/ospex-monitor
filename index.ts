f2f2142d5732bc0c464fdbd12b29e5531af874de
Vince Laird <vincelaird@gmail.com>
2023-03-29 23:17:52 -0700

    update package.json

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import express, { Express, Request, Response } from 'express';
-// import {config} from "./config";
 import { monitor } from './src/monitor';
 
 const app: Express = express();
@@ -7,11 +6,6 @@ const app: Express = express();
 app.get('/', (req: Request, res: Response) => {
     res.send('ospex server');
 });
-
-// app.listen(config.server.port, () => {
-//     monitor();
-//     return console.log(`[server]: Server is running on ${config.server.port}`);
-// });
 
 const PORT = process.env.PORT || 8001;
 
